b8c49a885c55f7659ac8865bb5f3c4f44b36c943
JyotishaChaurasia <jyotishachaurasia@gmail.com>
2023-09-17 14:08:44 +0530

    88 Merge Sorted Array

--- Count Symmetric Integers.py
+++ Count Symmetric Integers.py
@@ -1,15 +1,2 @@
-low = 1
-high = 100
-CountOfTotalSymmetricNum = 0
-for i in range(low, high+1):
-    
-    n = i
-    res = [int(x) for x in str(n)]
-    if len(res)%2 == 0:
-        q = len(res)//2
-        sum1 = sum(res[:q])
-        sum2 = sum(res[q:])
-        if sum1==sum2:
-            CountOfTotalSymmetricNum +=1
-print(CountOfTotalSymmetricNum)
+
 
